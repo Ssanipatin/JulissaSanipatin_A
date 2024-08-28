fe030f634c8b22ef9bc5adac3bfb3c17d72bb6dc
l1 <l1@UNO>
2024-08-27 21:42:24 -0500

    primera copia del repositorio

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+import java.awt.SystemColor;
+import java.util.Scanner;
+
+/**
+ *
+ * @author l1
+ */
+public class Aplicacion{
+    static Scanner leer = new Scanner(System.in)
+}
+public static void ejercicio_1(){
+
+}
